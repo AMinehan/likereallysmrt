a2cef994bb86be62db44d7030464ea92d632223a
AMinehan <soloin@gmail.com>
2018-02-07 00:36:14 -0800

    readies for prod

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ app.use(express.static('../client'));
 app.use(express.json())
 require('./config/routes.js')(app, express);
 
-app.listen(8080, () => {
+app.listen(4001, () => {
   console.log('am has listening on 8080')
 });
